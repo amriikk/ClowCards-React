a43efcbe8049f3c2ced27a10f906938225a33aaf
John Trujillo <amriikk@gmail.com>
2021-02-06 02:25:19 -0800

    update Deck component

---
+++ src/components/Deck.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Deck = ({ cards }) => {
+    <div className='section-center'>
+        {cards.map((clowCard) => {
+            const { id, title, img, desc, price } = clowCard;
+            return (
+                <article className='clow-card'>
+                    <img src={img} alt={title} className='photo'/>
+                    <div className='card-info'>
+                        <header>
+                            <h4>{ title }</h4>
+                            <h4 className='price'>{ price }</h4>
+                        </header>
+                        <p className='card-text'>{ desc }</p>
+                    </div>
+                </article>
+            );
+        })}
+    </div>
+};
+
+export default Deck;
