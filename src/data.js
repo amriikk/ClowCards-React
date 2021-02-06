ec2f679b1cffc8eb3c9d0d8879acb9e719e6c614
John Trujillo <amriikk@gmail.com>
2021-02-06 02:11:29 -0800

    update data.js

---
+++ src/data.js
@@ -0,0 +1,60 @@
+//Seeded Data for Menu items
+
+const deck = [
+    {
+      id: 1,
+      title: 'the dark',
+      category: 'dark',
+      price: 15.99,
+      // img: './images/item-1.jpeg',
+      img: 'https://i.imgur.com/yTJgibz.jpeg',
+      desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+    },
+    {
+      id: 2,
+      title: 'the light',
+      category: 'light',
+      price: 13.99,
+      // img: './images/item-2.jpeg',
+      img: 'https://i.imgur.com/Jut3y6l.jpeg',
+      desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+    },
+    {
+      id: 3,
+      title: 'the watery',
+      category: 'water',
+      price: 6.99,
+      // img: './images/item-3.jpeg',
+      img: 'https://i.imgur.com/t6xNpAx.jpeg',
+      desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+    },
+    {
+      id: 4,
+      title: 'the firey',
+      category: 'fire',
+      price: 20.99,
+      // img: './images/item-4.jpeg',
+      img: 'https://i.imgur.com/uwTRmqf.jpeg',
+      desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+    },
+    {
+      id: 5,
+      title: 'the windy',
+      category: 'wind',
+      price: 22.99,
+      // img: './images/item-5.jpeg',
+      img: 'https://i.imgur.com/3xZabqi.jpeg',
+      desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+    },
+    {
+      id: 6,
+      title: 'the earthy',
+      category: 'earth',
+      price: 18.99,
+      // img: './images/item-6.jpeg',
+      img: 'https://i.imgur.com/T7DqR79.jpeg',
+      desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+    },
+  ];
+
+  export default menu;
