5890281eaa1e5d5016348600b18063cf5815b145
John Trujillo <amriikk@gmail.com>
2021-02-15 22:31:59 -0800

    update Categories prop variable

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -4,14 +4,14 @@ const Deck = ({ cards }) => {
     return (
     <div className='section-center'>
         {cards.map((clowCard) => {
-            const { id, title, img, desc, price } = clowCard;
+            const { id, title, img, desc, guardian } = clowCard;
             return (
                 <article key={id} className='clow-card'>
                     <img src={img} alt={title} className='photo'/>
                     <div className='card-info'>
                         <header>
                             <h4>{ title }</h4>
-                            <h4 className='price'>{ price }</h4>
+                            <h4 className='guardian'>{ guardian }</h4>
                         </header>
                         <p className='card-text'>{ desc }</p>
                     </div>
